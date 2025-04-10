1a13de9fa71f3a5841515b65506eef919bf59752
Omar Tiffer <79622899+omartiffer@users.noreply.github.com>
2025-04-09 23:30:52 -0400

    Fix code formatter configuration

--- src/components/MobileNav/script.ts
+++ src/components/MobileNav/script.ts
@@ -4,10 +4,12 @@ const closeButton = document.getElementById("close-button");
 
 // Close the mobile menu after view transition
 document.addEventListener("astro:after-swap", () => {
-    closeMobileNav();
+  closeMobileNav();
 });
 
 // Re-attach the event listener after view transition
 document.addEventListener("astro:page-load", () => {
-    closeButton?.addEventListener("click", () => { closeMobileNav(); });
+  closeButton?.addEventListener("click", () => {
+    closeMobileNav();
+  });
 });
--- src/utils/mobileNav.ts
+++ src/utils/mobileNav.ts
@@ -2,20 +2,19 @@ const mobileNav = document.querySelector("#nav-mobile");
 const container = document.querySelector("#global-container");
 
 const openMobileNav = () => {
-
-    mobileNav?.classList.remove("translate-x-full");
-    mobileNav?.classList.add("-translate-x-full");
-    container?.classList.add("blur");
-    container?.classList.add("pointer-events-none");
-    document.documentElement.classList.add("overflow-hidden");
-}
+  mobileNav?.classList.remove("translate-x-full");
+  mobileNav?.classList.add("-translate-x-full");
+  container?.classList.add("blur");
+  container?.classList.add("pointer-events-none");
+  document.documentElement.classList.add("overflow-hidden");
+};
 
 const closeMobileNav = () => {
-    mobileNav?.classList.remove("-translate-x-full");
-    mobileNav?.classList.add("translate-x-full");
-    container?.classList.remove("blur");
-    container?.classList.remove("pointer-events-none");
-    document.documentElement.classList.remove("overflow-hidden");
-}
+  mobileNav?.classList.remove("-translate-x-full");
+  mobileNav?.classList.add("translate-x-full");
+  container?.classList.remove("blur");
+  container?.classList.remove("pointer-events-none");
+  document.documentElement.classList.remove("overflow-hidden");
+};
 
 export { openMobileNav, closeMobileNav };
