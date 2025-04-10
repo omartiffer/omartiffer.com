cf7599c300852da07979b54ae1173b14f6470644
Omar Tiffer <79622899+omartiffer@users.noreply.github.com>
2025-04-10 17:11:56 -0400

    Create new HeaderNav component

--- src/scripts/components/header.ts
+++ src/scripts/components/header.ts
@@ -1,16 +1,6 @@
 import { openMobileNav } from "@scripts/utils/mobileNav";
-import setTheme from "@scripts/utils/theme";
 
 const hamburguerButton = document.getElementById("hamburguer-button");
-const menuItems = document.querySelectorAll("#menu-item");
-
-menuItems.forEach((item) => {
-  item.addEventListener("click", () => {
-    menuItems.forEach((i) => i.classList.remove("text-primary"));
-    item.classList.add("text-primary");
-  }
-  );
-});
 
 hamburguerButton?.addEventListener("click", () => {
   openMobileNav();
