4af250f6e678635face224e1be62860667a68998
Omar Tiffer <79622899+omartiffer@users.noreply.github.com>
2025-04-10 17:11:17 -0400

    Refactor Social component

---
+++ src/config/social.ts
@@ -0,0 +1,40 @@
+import GitHub from "@assets/icons/github.svg";
+import LinkedIn from "@assets/icons/linkedin.svg";
+import X from "@assets/icons/x.svg";
+import Gmail from "@assets/icons/gmail.svg";
+
+const socialIcons = [{
+    title: "GitHub",
+    Icon: GitHub,
+    fill: "base-content",
+    width: 22,
+    height: 22,
+    url: "https://github.com/oatiffer"
+},
+{
+    title: "LinkedIn",
+    Icon: LinkedIn,
+    fill: "base-content",
+    width: 20,
+    height: 20,
+    url: "https://www.linkedin.com/in/omartiffer/"
+},
+{
+    title: "X",
+    Icon: X,
+    fill: "base-content",
+    width: 20,
+    height: 20,
+    url: "https://x.com/omartiffer"
+},
+{
+    title: "Gmail",
+    Icon: Gmail,
+    fill: "currentColor",
+    width: 22,
+    height: 22,
+    url: "mailto:me@omartiffer.cloud"
+}
+];
+
+export default socialIcons;
