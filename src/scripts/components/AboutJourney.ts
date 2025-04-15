78334691ad4cab3cf3141b7a0e3448029b130178
Omar Tiffer <79622899+omartiffer@users.noreply.github.com>
2025-04-15 18:10:31 -0400

    Add expand/collapse interactivity to the timeline

---
+++ src/scripts/components/AboutJourney.ts
@@ -0,0 +1,29 @@
+document.addEventListener("astro:page-load", () => {
+    const shadow = document.getElementById("shadow") as HTMLDivElement;
+    const readMoreBtn = document.getElementById(
+        "read-more"
+    ) as HTMLButtonElement;
+    const readLessBtn = document.getElementById(
+        "read-less"
+    ) as HTMLButtonElement;
+    const timeline = document.getElementById(
+        "timeline"
+    ) as HTMLDivElement;
+
+    let collapsedHeight: number;
+
+    readMoreBtn &&
+        readMoreBtn.addEventListener("click", () => {
+            collapsedHeight = timeline.clientHeight;
+
+            const expandedHeight = timeline.scrollHeight;
+            timeline.style.height = `${expandedHeight}px`;
+            shadow.style.visibility = "hidden";
+        });
+
+    readLessBtn &&
+        readLessBtn.addEventListener("click", () => {
+            timeline.style.height = `${collapsedHeight}px`;
+            shadow.style.visibility = "visible";
+        });
+});
