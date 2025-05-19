2d65e8d00e5f770eea095e3bce314e865ccb83a8
Omar Tiffer <79622899+omartiffer@users.noreply.github.com>
2025-05-18 23:59:34 -0400

    Add sitemap and robots

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+  site: "https://omartiffer.cloud",
+  integrations: [sitemap()],
 });
+
